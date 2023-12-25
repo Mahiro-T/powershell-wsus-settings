a44ce55d7cc83a662c15d62169825b81f3eb46bc
Mahiro <mahirokomatuo0102@gmail.com>
2023-12-26 00:01:05 +0900

    JSONからファイルを読み込んでWSUSの製品一覧を表示するようにした

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
 
+export default function Home() {
+  const jsonData =  Deno.readTextFileSync('json/wsus-products.json'); // JSONファイルのパスを指定する
+const json = JSON.parse(jsonData);
   return (
-    <div className="px-4 py-8 mx-auto bg-[#86efac]">
-    </div>
+      <div>
+          <h1>WSUSで配信可能な製品一覧</h1>
+          <div>
+              使用したWindowsのエディションとか<br/>
+              Edition Windows Server 2022 Datacenter Azure Edition<br/>
+              Version 21H2<br/>
+              Installed on 12/24/2023<br/>
+              OS build 20348.2159<br/>
+          </div>
+          <table>
+              <thead>
+              <tr>
+                  {/* Replace with your actual column names */}
+                  <td>製品名</td>
+                  <td>製品ID</td>
+                  <td>説明</td>
+                  {/* Add more columns as needed */}
+              </tr>
+              </thead>
+              <tbody>
+              {json.map((item:any, index:number) => (
+                  <tr key={index}>
+                      {/* Replace with your actual data keys */}
+                      <td>{item.Product.Title}</td>
+                      <td>{item.Product.Id}</td>
+                      <td>{item.Product.Description}</td>
+                      {/* Add more data keys as needed */}
+                  </tr>
+              ))}
+              </tbody>
+          </table>
+      </div>
   );
 }
