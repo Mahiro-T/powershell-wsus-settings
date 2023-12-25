648f5a89b3d33fbd63f04c3333802a81749d359e
Mahiro <mahirokomatuo0102@gmail.com>
2023-12-26 00:04:39 +0900

    Updated app title in _app.tsx file
    The change made updates the previous title 'powershell-wsus-settings' on the _app.tsx file with a new title, which is in Japanese ('WSUSで配信可能な製品一覧'). This modification reflects the improvements made on displaying list of products deliverable by WSUS from reading JSON files.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>powershell-wsus-settings</title>
+        <title>WSUSで配信可能な製品一覧</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
